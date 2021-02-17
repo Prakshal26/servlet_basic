04f30e645bd4b1a05538edeb06fee874a07f3aa7
Prakshal <jprakshal2607@gmail.com>
2021-02-17 22:54:43 +0530

    t

--- src/com/servletjsp/AddServlet.java
+++ src/com/servletjsp/AddServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+//test
 public class AddServlet extends HttpServlet {
 
     //Service is HttpServlet method class
